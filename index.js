9ebad9caa490f685f4ac77bd33ade45cfda7a954
Vladimir D <ande.vladimir@gmail.com>
2018-08-06 22:51:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function removeLastKitten(){
   return kittens.slice(0, kittens.length-1);
 }
 function removeFirstKitten(){
-  return kittens.slice(1, kittens.length)
+  return kittens.slice(1)
 }
